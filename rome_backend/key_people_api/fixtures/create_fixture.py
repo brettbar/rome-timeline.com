868cb158b623457993231b3073adf9abb9e5deee
brettbar <brettbarinaga@gmail.com>
2020-02-23 13:53:36 -0800

    key people now being pulled from backend

--- rome_backend/key_people_api/fixtures/create_fixture.py
+++ rome_backend/key_people_api/fixtures/create_fixture.py
@@ -1,15 +1,21 @@
 import json
 
 with open('./data.json') as f:
-  data = json.load(f)
+  json_data = json.load(f)
 f.close()
 
-fix_data = {}
+fix_data = []
 
-for item in data:
-  print(item)
+for item in json_data:
+  new_json = {}
+  new_json['model'] = 'key_people_api.person'
+  new_json['pk'] = json_data.index(item) + 1
+  new_json['fields'] = item
+  fix_data.append(new_json)
+
 
 with open('./fixture_data.json', 'w') as fix_f:
+  print(fix_data)
   json.dump(fix_data, fix_f)
 
 fix_f.close()
--- rome_backend/key_people_api/views.py
+++ rome_backend/key_people_api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 from rest_framework import viewsets
 from .serializers import PersonSerializer
 from .models import Person
@@ -7,5 +8,27 @@ from .models import Person
 # Create your views here.
 
 class PersonViewSet(viewsets.ModelViewSet):
-  queryset = Person.objects.all().order_by('name')
+  queryset = Person.objects.all().order_by('pk')
   serializer_class = PersonSerializer
+  # permission_classes = [IsPersonAdminOrReadOnly]
+
+  # def get_permissions(self):
+  #   return super().get_permissions()
+
+  # def get_queryset(self):
+  #   return Person.objects.all()
+
+  # def create(self, request):
+  #   pass
+
+  # def retrieve(self, request, pk=None):
+  #   pass
+
+  # def update(self, request, pk=None):
+  #   pass
+
+  # def partial_update(self, request, pk=None):
+  #   pass
+
+  # def destroy(self, request, pk=None):
+  #   pass
